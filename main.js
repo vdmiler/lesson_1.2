f411eff252a13aa74568f2c93bb35f0383ad0740
Miler Vadim <vdmiler1985@gmail.com>
2020-08-19 14:01:42 +0300

    completed task 1

---
+++ main.js
@@ -0,0 +1,11 @@
+// Task 1
+
+let name = 'Vadim',
+   age = 35,
+   isMarried = true,
+   haveChildren = true;
+
+console.log(typeof name, name);
+console.log(typeof age, age);
+console.log(typeof isMarried, isMarried);
+console.log(typeof haveChildren, haveChildren);
